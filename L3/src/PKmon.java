d37def48b9be4d311df6f93327bf4a5de2660f3d
mr-brightsid3 <banot.jan@gmail.com>
2022-04-10 17:35:37 +0200

    add first draft solution to Lab3/Zad1

--- L3/src/PKmon.java
+++ L3/src/PKmon.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 class Producer extends Thread {
     private Buffer _buf;
 
@@ -19,12 +22,55 @@ class Consumer extends Thread {
 }
 
 class Buffer {
-    public void put(int i) {
+    private final int[] array; // array storing queue elements
+    private int front; // front points to the first element in the queue
+    private int rear; // rear points to the last element in the queue
+    private int count; // current size of the queue
+    private final int capacity; // maximum capacity of the queue
 
+    Buffer(int size) {
+        array = new int[size];
+        capacity = size;
+        front = 0;
+        rear = -1;
+        count = 0;
     }
 
-    public int get() {
-        return 1;
+    public synchronized void put(int i) {
+        while (!isEmpty()) {
+            try {
+                wait();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        rear = (rear + 1) % capacity;
+        array[rear] = i;
+        count++;
+        notifyAll();
+    }
+
+    public synchronized int get() {
+        while (isEmpty()) {
+            try {
+                wait();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        int result = array[front];
+        front = (front + 1) % capacity;
+        count--;
+        notifyAll();
+        return result;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public Boolean isEmpty() {
+        return (size() == 0);
     }
 }
 
